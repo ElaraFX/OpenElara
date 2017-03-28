3f6aeb06a0bc460d97263bd8ea91ae624f042aaf
WindyPaper <windy_max@163.com>
2017-03-28 17:39:41 +0800

    add comment to example

--- er/export_ess_example.cpp
+++ er/export_ess_example.cpp
@@ -25,24 +25,36 @@
 
 int main()
 {
+	/* 坐标系规则跟矩阵
+	Elara uses a right-handed coordinate system, with row-major matrices.
+	For world space and object space, Z axis points up.
+	For camera space, camera direction points towards negative direction of Z axis,
+	X axis points to the right, and Y axis points up.
+	https://github.com/ElaraFX/elaradoc/wiki/Nodes
+	*/
+
+	/* 输入的文件名都不支持中文 */
 	EH_Context *pContext = EH_create();
 	EH_ExportOptions option;
 	option.base85_encoding = false;
 	option.left_handed = false;
-	EH_begin_export(pContext, "test.ess", &option);
+	EH_begin_export(pContext, "test.ess", &option); /* ess是本地生成路径 */
 
 	/**< 设置渲染参数 */
 	EH_RenderOptions render_op;
-	render_op.quality = EH_MEDIUM;
+	render_op.quality = EH_MEDIUM; /**< 中等渲染品质，ElaraHomeAPI头文件里面有定义品质 */
 	EH_set_render_options(pContext, &render_op);
 
-	/**< 创建摄像机 */
+	/**< 创建摄像机
+		API暴露的接口中只支持投影摄像机
+	*/
 	EH_Camera cam;
 	cam.fov = 45;
 	cam.near_clip = 0.01f;
 	cam.far_clip = 1000.0f;
 	cam.image_width = 640;
 	cam.image_height = 480;
+	/* 摄像机变换矩阵 行优先的方式 */
 	eiMatrix cam_tran = ei_matrix(0.731353, -0.681999, -0.0, 0.0, 0.255481, 0.27397, 0.927184, 0.0, -0.632338, -0.678099, 0.374607, 0.0, -38.681263, -49.142731, 21.895681, 1.0);
 	memcpy(cam.view_to_world, &cam_tran.m[0][0], sizeof(EH_Mat));
 	EH_set_camera(pContext, &cam);
@@ -61,7 +73,7 @@ int main()
 		0.0,0.0, 1.0,0.0, 1.0,1.0, 0.0,1.0,
 		0.0,0.0, 1.0,0.0, 1.0,1.0, 0.0,1.0
 	};
-	uint_t indice[36] = {
+	uint_t indice[36] = { /* 顶点索引 */
 		0,2,1,
 		0,3,2,
 
@@ -96,11 +108,11 @@ int main()
 
 	/**< 设置方向光 */
 	EH_Sun sun;
-	sun.dir[0] = 0;
-	sun.dir[1] = 0;
+	sun.dir[0] = 0; /* theta */
+	sun.dir[1] = 0; /* phi  头文件有具体描述*/
 	float color[3] = {1, 1, 1};
 	memcpy(sun.color, color, sizeof(color));
-	sun.intensity = 3.1;
+	sun.intensity = 3.1; /* 强度0 - (float)最大值 */
 	EH_set_sun(pContext, &sun);
 
 	/**< 关联 网格信息 和 材质 */
@@ -108,35 +120,35 @@ int main()
 	EH_MeshInstance inst;
 	inst.mesh_name = mesh_name;
 	inst.mtl_name = simple_mtl;
-	eiMatrix inst_tran = ei_matrix(
+	eiMatrix inst_tran = ei_matrix( /* 关联材质网络的变化矩阵 */
 		1, 0, 0, 0,
 		0, 1, 0, 0,
 		0, 0, 1, 0,
 		0, 0, 0, 1
-	);
+		);
 	memcpy(inst.mesh_to_world, inst_tran.m, sizeof(inst.mesh_to_world));
 	EH_add_mesh_instance(pContext, simple_inst_name, &inst);
 
 	/**< 加载外部ESS */
-	eiMatrix include_ess_mat = ei_matrix(
+	eiMatrix include_ess_mat = ei_matrix( /* 引用外部ESS模型的变化矩阵 */
 		1, 0, 0, 0,
 		0, 1, 0, 0,
 		0, 0, 1, 0,
 		0, 200, -10, 1
 		);
 	EH_AssemblyInstance include_inst;
-	include_inst.filename = "default.ess";
+	include_inst.filename = "default.ess"; /* 需要包含的ESS */
 	memcpy(include_inst.mesh_to_world, include_ess_mat.m, sizeof(include_inst.mesh_to_world));
-	EH_add_assembly_instance(pContext, "include_test_ess", &include_inst);
+	EH_add_assembly_instance(pContext, "include_test_ess", &include_inst); /* include_test_ess 是ESS中节点的名字 不能重名 */
 
-	EH_end_export(pContext);
+	EH_end_export(pContext); /* 保存ESS文件 */
 
 	/**< 设置渲染时候的回调 */
 	//EH_set_progress_callback(pContext, (EH_ProgressCallback)test_get_progress_cb);
 	//EH_set_display_callback(pContext, (EH_display_callback)test_display_cb);
 	//EH_start_render(pContext, "default.ess", false);
 
-	EH_delete(pContext);
+	EH_delete(pContext); /* 释放内存，非必须 */
 
 	return 0;
 }
