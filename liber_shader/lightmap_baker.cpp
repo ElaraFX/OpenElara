a944e3e9cb9d5f9b54cfce6d051a8ca1617b5720
Elvic <43492276@qq.com>
2017-05-09 21:46:50 +0800

    Update legal head to align with our core.

--- liber_shader/lightmap_baker.cpp
+++ liber_shader/lightmap_baker.cpp
@@ -1,3 +1,18 @@
+/**************************************************************************
+ * Copyright (C) 2016 Rendease Co., Ltd.
+ * All rights reserved.
+ *
+ * This program is commercial software: you must not redistribute it 
+ * and/or modify it without written permission from Rendease Co., Ltd.
+ *
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * End User License Agreement for more details.
+ *
+ * You should have received a copy of the End User License Agreement along 
+ * with this program.  If not, see <http://www.rendease.com/licensing/>
+ *************************************************************************/
 
 #include <ei_shaderx.h>
 #include <ei_fixed_pool.h>
